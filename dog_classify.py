b52cb9cb33dddb20ced3ffef95ae117313dd576c
hangeulBae <dxdiag20@gmail.com>
2018-06-29 11:00:57 +0900

    dog_classify: unnecessary code deleted

--- dog_classify.py
+++ dog_classify.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 26 07:16:04 2018
-
-@author: raghav prabhu
-Re-modified TensorFlow classification file according to our need.
+Created on Fri June 29 07:16:04 2018
 """
+
 import tensorflow as tf
 import sys
 import os
-import csv
 
 # Disable tensorflow compilation warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -19,28 +16,20 @@ Classify images from test folder and predict dog breeds along with score.
 '''
 
 
-#def classify_image(image_path, headers):
 def classify_image(image_path):
-    # f = open('submit.csv', 'w')
-    # writer = csv.DictWriter(f, fieldnames=headers)
-    # writer.writeheader()
-
     # Loads label file, strips off carriage return
     label_lines = [line.rstrip() for line
-                   in tf.gfile.GFile("/Users/hangeulbae/Desktop/retrained_labels.txt")]
+                   in tf.gfile.GFile("/Users/hangeulbae/Desktop/dog_labels.txt")]
 
     # Unpersists graph from file
-    with tf.gfile.FastGFile("/Users/hangeulbae/Desktop/retrained_graph.pb", 'rb') as f:
+    with tf.gfile.FastGFile("/Users/hangeulbae/Desktop/dog_graph.pb", 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
 
-    #files = os.listdir(image_path)
     file = image_path
     with tf.Session() as sess:
-        # for file in files:
         # Read the image_data
-        #image_data = tf.gfile.FastGFile(image_path + '/' + file, 'rb').read()
         image_data = tf.gfile.FastGFile(image_path, 'rb').read()
         # Feed the image_data as input to the graph and get first prediction
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
@@ -50,7 +39,6 @@ def classify_image(image_path):
 
         # Sort to show labels of first prediction in order of confidence
         top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-        records = []
         row_dict = {}
         head, tail = os.path.split(file)
         row_dict['id'] = tail.split('.')[0]
@@ -79,28 +67,16 @@ def classify_image(image_path):
             print('%s (score = %.5f)' % (human_string, score))
             row_dict[human_string] = score
             for node_id in top_k:
-                human_string = label_lines[node_id]
                 score = predictions[0][node_id]
                 if (maxScore < score):
                     index = node_id
                     maxScore = score
-        records.append(row_dict.copy())
         print("what she looks like?")
         print('%s (score = %.5f)' % (label_lines[index], maxScore))
-        #writer.writerows(records)
-
     f.close()
 
 
 def main():
-    # test_data_folder = 'test'
-    #
-    # template_file = open('sample_submission.csv', 'r')
-    # d_reader = csv.DictReader(template_file)
-
-    # get fieldnames from DictReader object and store in list
-    # headers = d_reader.fieldnames
-    # template_file.close()
     classify_image('/Users/hangeulbae/Desktop/jung.jpg')
 
 
