957bac897898e9db49882b6f5f3e2a92b0fa09fc
hangeulBae <dxdiag20@gmail.com>
2018-06-29 12:07:56 +0900

    dog_classify: add sys parameter code

--- dog_classify.py
+++ dog_classify.py
@@ -19,14 +19,15 @@ Classify images from test folder and predict dog breeds along with score.
 def classify_image(image_path):
     # Loads label file, strips off carriage return
     label_lines = [line.rstrip() for line
-                   in tf.gfile.GFile("/Users/hangeulbae/Desktop/dog_labels.txt")]
+                   in tf.gfile.GFile("dog_labels.txt")]
 
     # Unpersists graph from file
-    with tf.gfile.FastGFile("/Users/hangeulbae/Desktop/dog_graph.pb", 'rb') as f:
+    with tf.gfile.FastGFile("dog_graph.pb", 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
-
+    index = 0
+    maxScore = 0
     file = image_path
     with tf.Session() as sess:
         # Read the image_data
@@ -42,8 +43,6 @@ def classify_image(image_path):
         row_dict = {}
         head, tail = os.path.split(file)
         row_dict['id'] = tail.split('.')[0]
-        index = 0
-        maxScore = 0
         for node_id in top_k:
             human_string = label_lines[node_id]
 
@@ -64,21 +63,22 @@ def classify_image(image_path):
             if (human_string == 'flat_coated_retriever'):
                 human_string = 'flat-coated_retriever'
             score = predictions[0][node_id]
-            print('%s (score = %.5f)' % (human_string, score))
+            #print('%s (score = %.5f)' % (human_string, score))
             row_dict[human_string] = score
             for node_id in top_k:
                 score = predictions[0][node_id]
                 if (maxScore < score):
                     index = node_id
                     maxScore = score
-        print("what she looks like?")
-        print('%s (score = %.5f)' % (label_lines[index], maxScore))
+        #print("what she looks like?")
+        #print('%s (score = %.5f)' % (label_lines[index], maxScore))
     f.close()
+    return label_lines[index]
 
 
 def main():
-    classify_image('/Users/hangeulbae/Desktop/jung.jpg')
-
+    #classify_image('/Users/hangeulbae/Desktop/jung.jpg')
+    classify_image(sys.argv[1])
 
 if __name__ == '__main__':
     main()
